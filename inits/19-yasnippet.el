5a0f5efa9612b42bd4125c9fd201cc85db04c1eb
Tomoki Imai <tomo832@gmail.com>
2014-11-03 15:17:13 +0900

    Add snippets module

--- inits/19-yasnippet.el
+++ inits/19-yasnippet.el
@@ -1,5 +1,9 @@
 (require 'yasnippet)
-(yas-reload-all) 
+(setq yas-snippet-dirs '("~/.emacs.d/tomoki-snippets"))
+(yas-reload-all)
+(define-key yas-minor-mode-map (kbd "C-;") 'yas/expand)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
+(define-key yas-minor-mode-map [(tab)] nil)
 (add-hook 'prog-mode-hook
           '(lambda ()
              (yas-minor-mode)))
