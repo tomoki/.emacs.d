79ef71d3e3144b676c4d44e10c320b89eeb2d978
Tomoki Imai <tomo832@gmail.com>
2014-12-23 02:26:54 +0900

    rmeove olds

---
+++ elpa/icomplete+-20141122.1832/icomplete+-pkg.el
@@ -0,0 +1,1 @@
+(define-package "icomplete+" "20141122.1832" "Extensions to `icomplete.el'." 'nil :url "http://www.emacswiki.org/icomplete+.el" :keywords '("help" "abbrev" "internal" "extensions" "local" "completion" "matching"))
