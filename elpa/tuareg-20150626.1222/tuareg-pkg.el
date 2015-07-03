23ebdf1a2b5dd3325712135ad6196d435611790b
Tomoki Imai <tomo832@gmail.com>
2015-07-03 18:38:09 +0900

    update elpa

---
+++ elpa/tuareg-20150626.1222/tuareg-pkg.el
@@ -0,0 +1,7 @@
+(define-package "tuareg" "20150626.1222" "OCaml mode for Emacs."
+  '((caml "3.12.0.1"))
+  :url "https://github.com/ocaml/tuareg" :keywords
+  '("ocaml" "languages"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
