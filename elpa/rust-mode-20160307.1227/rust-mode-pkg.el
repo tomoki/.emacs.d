74cfa788b77df0fe44a4594d26d023a58630afee
Tomoki Imai <tomo832@gmail.com>
2016-04-07 09:47:11 +0900

    update

--- elpa/rust-mode-20160307.1227/rust-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "rust-mode" "20160307.1227" "A major emacs mode for editing Rust source code" 'nil :url "https://github.com/rust-lang/rust-mode" :keywords '("languages"))
--- elpa/seq-1.11/seq-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; seq-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("seq.el") (22172 17631 396304 242000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; seq-autoloads.el ends here
