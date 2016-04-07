5bf9be3d6208db28fd8b4bdaf2a8eaeabd9930f5
Tomoki Imai <tomo832@gmail.com>
2016-04-07 09:48:44 +0900

    Avoid eshell bugs

--- inits/10-shell.el
+++ inits/10-shell.el
@@ -1,12 +1,11 @@
 ;; 補完時に大文字小文字を区別しない
-(setq eshell-cmpl-ignore-case t)
-;; 確認なしでヒストリ保存
+;; FIXME: It is Emacs bug, but it is sometime buggy.
+;; (setq eshell-cmpl-ignore-case t)
+
+
 (setq eshell-ask-to-save-history (quote always))
-;; 補完時にサイクルする
 (setq eshell-cmpl-cycle-completions t)
-;;補完候補がこの数値以下だとサイクルせずに候補表示
 (setq eshell-cmpl-cycle-cutoff-length 5)
-;; 履歴で重複を無視する
 (setq eshell-hist-ignoredups t)
 (setq eshell-banner-message "")
 (defun my-clear ()
