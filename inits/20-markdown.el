17ebc5e2108305264e36bedacbb8e3b2b1aad75d
Tomoki Imai <tomo832@gmail.com>
2016-04-18 10:31:03 +0900

    Add advice for <return> in markdown

--- inits/20-markdown.el
+++ inits/20-markdown.el
@@ -1,3 +1,10 @@
 ;; http://jblevins.org/projects/markdown-mode/
 (add-hook 'markdown-mode-hook
           '(lambda () (electric-indent-mode 0)))
+
+;; https://www.yewton.net/2016/01/20/markdown-mode-skk-kakutei/
+(defun my--markdown-entery-key-ad (this-func &rest args)
+  "markdown-modeでskk-henkan-mode中にエンターすると行頭にカーソルが飛んでしまう問題の対応"
+  (if skk-henkan-mode (skk-kakutei)
+    (apply this-func args)))
+(advice-add #'markdown-enter-key :around #'my--markdown-entery-key-ad)
