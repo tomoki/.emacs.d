bc466b33a7703daa725bdb3ec45111fd88a19e92
Tomoki Imai <tomo832@gmail.com>
2017-06-18 18:32:19 +0900

    Update

---
+++ elpa/icomplete+-20170303.1456/icomplete+-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "icomplete+" "20170303.1456" "Extensions to `icomplete.el'." 'nil :url "https://www.emacswiki.org/emacs/download/icomplete%2b.el" :keywords '("help" "abbrev" "internal" "extensions" "local" "completion" "matching"))
