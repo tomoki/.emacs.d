a879e9076ce594f80348577fbe26d7373346120f
Tomoki Imai <tomo832@gmail.com>
2017-09-05 22:40:03 +0900

    Update packages

--- elpa/markdown-mode-20170617.17/markdown-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "markdown-mode" "20170617.17" "Major mode for Markdown-formatted text" '((emacs "24") (cl-lib "0.5")) :commit "c1408d2b9ef2fba4723302e72f23eddaf1d3ecf4" :url "http://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
